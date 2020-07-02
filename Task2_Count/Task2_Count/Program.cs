a5929a2107d04cc55441f62d10639c805b863d49
Alexey Kulik <kpblc2000@gmail.com>
2020-07-02 18:49:09 +0300

    Подсчет количества цифр в числе

---
+++ Task2_Count/Task2_Count/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task2_Count
+{
+	class Program
+	{
+		/// <summary>
+		/// Алексей Кулик kpblc2000@yandex.ru
+		/// Урок 2, задача 2
+		/// Написать метод подсчета количества цифр числа
+		/// </summary>
+		static void Main()
+		{
+			int value = 12;
+			Console.WriteLine(NumRangeWhile(value));
+			Console.WriteLine(NumRangeString(value));
+			Console.ReadKey();
+		}
+
+		static int NumRangeString(int Value)
+		{
+			return Value.ToString().Length;
+		}
+
+		static int NumRangeWhile(int Value)
+		{
+			int res = 1;
+			while (Value / 10 > 0)
+			{
+				res++;
+				Value = Value / 10;
+			}
+			return res;
+		}
+
+	}
+}
