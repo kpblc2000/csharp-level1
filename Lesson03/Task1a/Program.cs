85f9b5f7415dca36289de213c44a3efd9dece39a
Alexey Kulik <kpblc2000@gmail.com>
2020-07-06 20:36:55 +0300

    Задача 1 (операции с комплексными числами)

---
+++ Lesson03/Task1a/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1a
+{
+	// <summary>
+	/// Алексей Кулик kpblc2000@yandex.ru
+	/// Урок 3, задача 1а
+	/// Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры.
+	/// </summary>
+
+	struct Complex
+	{
+		public float Real;
+		public float Im;
+
+		public Complex Plus(Complex Value)
+		{
+			Complex temp;
+			temp.Im = Im + Value.Im;
+			temp.Real = Real + Value.Real;
+			return temp;
+		}
+
+
+		public Complex Minus(Complex Value)
+		{
+			Complex temp;
+			temp.Im = -Value.Im;
+			temp.Real = -Value.Real;
+			return Plus(temp);
+		}
+
+		public override string ToString()
+		{
+			return Real + "+" + Im + "i";
+		}
+	}
+
+	class Program
+	{
+		static void Main()
+		{
+			Complex a = new Complex
+			{
+				Real = 10,
+				Im = 5
+			};
+			Complex b = new Complex
+			{
+				Real = 2,
+				Im = 3
+			};
+			Console.WriteLine("a = " + a); // ToString(), похоже, можно не прописывать
+			Console.WriteLine("b = " + b);
+			Console.WriteLine("a + b = " + a.Plus(b));
+			Console.WriteLine("a - b = " + a.Minus(b));
+			Console.WriteLine();
+
+			Console.WriteLine("Нажмите любую клавишу");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Lesson03/Task1c/Complex.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kpblc
+{
+	/// <summary>
+	/// Алексей Кулик kpblc2000@yandex.ru
+	/// Урок 3, задача 1б, 1в
+	/// Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.
+	/// </summary>
+	
+	class Complex
+	{
+		private double _real;
+		private double _im;
+
+		public Complex()
+		{
+			_real = 0.0;
+			_im = 0.0;
+		}
+		public Complex(double Real)
+		{
+			_real = Real;
+			_im = 0.0;
+		}
+		public Complex(double Real, double Im)
+		{
+			_real = Real;
+			_im = Im;
+		}
+		public double Real { get { return _real; } set { _real = value; } }
+		public double Im { get { return _im; } set { _im = value; } }
+		public override string ToString()
+		{
+			return "(" + _real + (_im < 0 ? "" : "+") + _im + "i)";
+		}
+		public Complex Plus(Complex Value)
+		{
+			return new Complex(Value.Real + _real, Value.Im + _im);
+		}
+		public Complex Plus(double Value)
+		{
+			return Plus(new Complex(Value));
+		}
+		public Complex Plus(int Value)
+		{
+			return Plus((double)Value);
+		}
+		public Complex Minus(Complex Value)
+		{
+			return new Complex(_real - Value.Real, _im - Value.Im);
+		}
+		public Complex Minus(double Value)
+		{
+			return Minus(new Complex(Value));
+		}
+		public Complex Minus(int Value)
+		{
+			return Minus((double)Value);
+		}
+		public Complex Multiple(Complex Value)
+		{
+			return new Complex(_real * Value.Real - _im * Value.Im, _real * Value.Im + Value.Real * _im);
+		}
+	}
+
+}
